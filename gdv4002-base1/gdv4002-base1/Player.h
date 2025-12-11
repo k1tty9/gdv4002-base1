67158ce13357e2029dbeea29ed140726051dd3ef
st20304265 <st20304265@outlook.cardiffmet.ac.uk>
2025-12-11 16:33:33 +0000

    section 7.2

--- gdv4002-base1/gdv4002-base1/Player.h
+++ gdv4002-base1/gdv4002-base1/Player.h
@@ -1,1 +1,16 @@
 #pragma once
+#include "GameObject2D.h"
+
+class Player : public GameObject2D {
+
+private:
+	float playerSpeed;
+
+public:
+	Player(glm::vec2 initPosition, float initOrientation, glm::vec2 initSize, GLuint initTextureID, float initialPlayerSpeed);
+
+	void update(double tDelta) override;
+
+
+};
+
